5f496804e15c7bb2a9a3d4fb66a3f119c8ae702c
JaredMontgomery <TheJaredMontgomery@gmail.com>
2024-07-15 22:36:46 -0400

    Implemented a stack in Python.

---
+++ python/stack.py
@@ -0,0 +1,31 @@
+class Stack:
+    """
+    Implements a stack data structure with a push() function, pop() function, and
+    support for len(). Internally, a list is used to store the stack items.
+    """
+
+    """
+    Creates a list to hold the items.
+    """
+    def __init__(self, items: list = []):
+        self.items = items
+    
+    """
+    Adds an item to the stack and returns the new list length.
+    """
+    def push(self, *args):
+        self.items.append(args)
+
+        return len(self.items)
+    
+    """
+    Removes an item from the stack and returns it.
+    """
+    def pop(self):
+        return self.items.pop()
+    
+    """
+    Gives the number of items in the stack.
+    """
+    def __len__(self):
+        return len(self.items)
