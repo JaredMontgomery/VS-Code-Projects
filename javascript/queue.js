0161cce64dbad55f683b1ab28e008d40292ad886
JaredMontgomery <TheJaredMontgomery@gmail.com>
2024-07-15 22:22:56 -0400

    Implemented a queue in Javascript.

---
+++ javascript/queue.js
@@ -0,0 +1,31 @@
+﻿/**
+ * A class that implements a queue data structure with an enqueue and dequeue
+ * function. Internally, it uses an array to store all of the data.
+ * 
+ * @author Jared Austin Montgomery.
+ */
+class Queue {
+    // Takes an array to use to store the queue items.
+    constructor(arr) {
+        this.arr = arr;
+    }
+
+    // Adds an item to the queue.
+    enqueue(...items) {
+        this.arr.push(...items);
+
+        return this.arr.length;
+    }
+
+    // Removes an item to the queue.
+    dequeue() {
+        let item = this.arr.shift();
+
+        return item;
+    }
+
+    // Gives the number of items in the queue.
+    length() {
+        return this.arr.length;
+    }
+}
