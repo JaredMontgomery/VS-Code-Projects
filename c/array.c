ebc689af197be2e121c5be6ed6b7a5f135626f17
JaredMontgomery <TheJaredMontgomery@gmail.com>
2024-08-29 15:52:16 -0400

    Added insert and delete operations to the array.

--- c/array.c
+++ c/array.c
@@ -7,14 +7,14 @@
 #include <stdlib.h>
 
 /**
- * Creates and returns n array of a certain size.
+ * Creates and returns n array of a certain length.
  * 
- * Time complexity - O(N).
+ * Time complexity - O(N), where N is [len].
  * Space complexity - O(N).
  */
-int* Array(size_t size)
+int* Array(size_t len)
 {
-    return malloc(size * sizeof(int));
+    return malloc(len * sizeof(int));
 }
 
 /**
@@ -40,19 +40,85 @@ int set(int* arr, size_t index, int item)
 }
 
 /**
+ * Inserts an int at an index in an array. The new int is returned. Items are
+ * shifted over to the right and any item at the end of the array is discarded.
+ * 
+ * Time complexity - O(N), where N is [len].
+ * Space complexity - O(1).
+ */
+int insert(int* arr, size_t index, size_t len, int item)
+{
+    // Shifts items at the index to the right.
+    for (int i = len - 1; i > index; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[index] = item;
+
+    return item;
+}
+
+/**
+ * Deletes an int at an index in an array. The removed int is returned. Items
+ * are shifted over to the left. The last item in the array will be empty (0).
+ * 
+ * Time complexity - O(N), where N is [len].
+ * Space complexity - O(1).
+ */
+int delete(int* arr, size_t index, size_t len)
+{
+    // Item that's getting removed.
+    int item = arr[index];
+
+    // Shifts items at the index to the left.
+    for (int i = index; i < len - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+
+    arr[len - 1] = 0;
+
+    return item;
+}
+
+/**
  * Tests the above functions by making an array, adding stuff, then printing.
  */
 int main(void)
 {
-    int* arr = Array(4);
+    // Creates an array.
+    int len = 4;
+    int* arr = Array(len);
     
-    for (size_t i = 0; i < 4; i++)
+    // Adds items.
+    for (size_t i = 0; i < len; i++)
     {
         set(arr, i, i + 1);
     }
 
-    for (size_t i = 0; i < 4; i++)
+    // Prints items.
+    for (size_t i = 0; i < len; i++)
     {
         printf("%d\n", get(arr, i));
     }
+    printf("\n");
+
+    insert(arr, 0, len, 10);
+
+    // Prints items to see the insertion.
+    for (size_t i = 0; i < len; i++)
+    {
+        printf("%d\n", get(arr, i));
+    }
+    printf("\n");
+
+    delete(arr, 0, len);
+
+    // Prints items to see the deletion.
+    for (size_t i = 0; i < len; i++)
+    {
+        printf("%d\n", get(arr, i));
+    }
+    printf("\n");
 }
