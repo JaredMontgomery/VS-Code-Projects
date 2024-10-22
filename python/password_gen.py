060f4241792c7d0a149752d1cdc56e406a29a5aa
JaredMontgomery <TheJaredMontgomery@gmail.com>
2024-10-21 22:13:31 -0400

    Added a password-generating program.

---
+++ python/password_gen.py
@@ -0,0 +1,58 @@
+def password_gen(length: int, upper: bool = True, lower: bool = True, numbers: bool = True, symbols: bool = True) -> str:
+    """
+    Summary:
+        Generates a password of a certain length with different options for the
+        chars used.
+
+        Time complexity - O(N), where N is [length].
+        Space complexity - O(N).
+
+    Parameters:
+        [length] - The number of characters in the password.
+
+        [upper] - Whether to include uppercase letters.
+        [lower] - Whether to include lowercase letters.
+        [digits] - Whether to include digits.
+        [symbols] - Whether to include symbols.
+    
+    Returns:
+        The aforementioned password.
+    """
+
+    from random import choice
+
+    # All of the characters that a password could have.
+    ascii_uppercase: str = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    ascii_lowercase: str = "qwertyuiopasdfghjklzxcvbnm"
+    digits: str = "1234567890"
+    symbols: str = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/"
+
+    # Will holds all of the chars for the password. Doing this so that a new str
+    # doesn't need to be made on each iteration, which would be slow.
+    password_chars: list[str] = [' '] * length
+    # Holds what kind of characters are enabled (what params are set to True).
+    options = []
+
+    # Adds the enabled options to a list.
+    for tup in enumerate([upper, lower, digits, symbols]):
+        if tup[1]:
+            options.append(tup[0])
+
+    # Adds every char to the password:
+    for i in range(length):
+        # Randomly picks what kind of char to put this time.
+        match choice(options):
+            case 0:
+                # Randomly picks 1 uppercase letter.
+                password_chars[i] = choice(ascii_uppercase)
+            case 1:
+                password_chars[i] = choice(ascii_lowercase)
+            case 2:
+                password_chars[i] = choice(digits)
+            case 3:
+                password_chars[i] = choice(symbols)
+    
+    # Creates and returns the password.
+    return "".join(password_chars)
+
+print(password_gen(10))
