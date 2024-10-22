3015b022bb2d18c82169880e1a7c40aa525f53a3
JaredMontgomery <TheJaredMontgomery@gmail.com>
2024-10-21 22:14:47 -0400

    Made program that wipes free memory space.

---
+++ python/delete.py
@@ -0,0 +1,21 @@
+def delete(char_count: int) -> None:
+    """
+    Summary:
+        If left running long enough, it will completely wipe all free space on
+        a computer by writing random data. This can be used to truly delete any
+        file.
+
+        Time complexity - O(N), where N is [char_count].
+        Space complexity - O(N).
+    
+    Parameters:
+        [char_count] - The number of characters to write to memory.
+    """
+
+    from random import choice
+    from string import printable
+
+    # Opens a file and keeps writingh junk to it.
+    with open("file.txt", "w") as file:
+        for i in range(char_count):
+            file.write(choice(printable))
