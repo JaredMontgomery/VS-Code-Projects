e521bd3ea5dd33a247423a890b9874231c852670
JaredMontgomery <TheJaredMontgomery@gmail.com>
2024-11-26 10:19:17 -0500

    Implemented a doubly-linked list in C.

--- c/linked_list.c
+++ c/linked_list.c
@@ -5,6 +5,8 @@
 typedef struct ListNode
 {
     int num;
+
+    struct ListNode* prev;
     struct ListNode* next;
 }
 ListNode;
@@ -16,8 +18,14 @@ typedef struct LinkedList
     ListNode* head;
     ListNode* tail;
 
+    // The number of nodes in the list.
+    int len;
+
     void (*insert_at_head)(struct LinkedList* list, int num);
     void (*insert_at_tail)(struct LinkedList* list, int num);
+
+    int (*remove_at_head)(struct LinkedList* list);
+    int (*remove_at_tail)(struct LinkedList* list);
 }
 LinkedList;
 
@@ -26,11 +34,37 @@ void insert_at_head(LinkedList* list, int num)
 {
     ListNode* new_node = malloc(sizeof(ListNode));
     new_node->num = num;
-    new_node->next = NULL;
-
-    // The new node points to the head and becomes the new head.
+    new_node->prev = NULL;
+    // The new node points to the head.
     new_node->next = list->head;
+
+    // The new node becomes the new head.
     list->head = new_node;
+
+    list->len++;
+}
+
+// This removes a node with a value at the head of a linked list and returns its
+// value.
+int remove_at_head(LinkedList* list)
+{
+    // Can't remove what isn't there.
+    if (list->len == 0)
+    {
+        return 0;
+    }
+
+    int old_num = list->head->num;
+
+    // Advances the head forwards.
+    list->head = list->head->next;
+
+    // Deletes the old head.
+    free(list->head->prev);
+
+    list->len--;
+
+    return old_num;
 }
 
 // This inserts a node with a value of [num] at the tail of a linked list.
@@ -38,39 +72,76 @@ void insert_at_tail(LinkedList* list, int num)
 {
     ListNode* new_node = malloc(sizeof(ListNode));
     new_node->num = num;
+    // The new node goes after the tail.
+    new_node->prev = list->tail;
     new_node->next = NULL;
 
-    // The new node goes after the tail and becomes the new tail.
     list->tail->next = new_node;
+    // The new node becomes the new tail.
     list->tail = new_node;
+
+    list->len++;
+}
+
+// This removes a node with a value at the tail of a linked list and returns its
+// value.
+int remove_at_tail(LinkedList* list)
+{
+    // Can't remove what isn't there.
+    if (list->len == 0)
+    {
+        return 0;
+    }
+
+    int old_num = list->tail->num;
+
+    // Advances the tail backwards.
+    list->tail = list->tail->prev;
+
+    // Deletes the old tail.
+    free(list->tail->next);
+
+    list->len--;
+
+    return old_num;
 }
 
 int main(void)
 {
-    // Creates a list and sets some stuff up.
+    // Creates a list and sets some stuff up:
+
     LinkedList* list = malloc(sizeof(LinkedList));
+
     list->insert_at_head = insert_at_head;
     list->insert_at_tail = insert_at_tail;
+
+    list->remove_at_head = remove_at_head;
+    list->remove_at_tail = remove_at_tail;
 
     // Creates the head node.
     list->head = malloc(sizeof(ListNode));
     list->head->num = 1;
     list->head->next = NULL;
 
+    list->len = 1;
+
     // Only 1 node for now, so the head and the tail are the same.
     list->tail = list->head;
 
+    list->insert_at_tail(list, 1);
     list->insert_at_tail(list, 2);
-    list->insert_at_head(list, 3);
+    list->insert_at_tail(list, 3);
     list->insert_at_tail(list, 4);
 
-    ListNode* node = list->head;
+    printf("%d", list->remove_at_head(list));
 
-    // Iterates over the list.
-    while (node != NULL)
-    {
-        printf("%d\n", node->num);
+    // ListNode* node = list->head;
 
-        node = node->next;
-    }
+    // // Iterates over the list.
+    // while (node != NULL)
+    // {
+    //     printf("%d\n", node->num);
+
+    //     node = node->next;
+    // }
 }
